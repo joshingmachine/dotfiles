571d65236a86e0e92920283ea0bf86f242c6c722
joshingmachine <josh@joshingmachine.com>
2018-02-26 01:37:23 -0500

    Adding install scripts, nvm, zshrc, etc

--- symlink.sh
+++ symlink.sh
@@ -2,9 +2,11 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s ${BASEDIR}/.vim/ ~/.vim
-ln -s ${BASEDIR}/.editorconfig ~/.editorconfig
-ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
-ln -s ${BASEDIR}/.gitignore ~/.gitignore
+ln -s ${BASEDIR}/.vim/ ${HOME}/.vim
+ln -s ${BASEDIR}/.editorconfig ${HOME}/.editorconfig
+ln -s ${BASEDIR}/.gitconfig ${HOME}/.gitconfig
+ln -s ${BASEDIR}/.gitignore ${HOME}/.gitignore
+ln -s ${BASEDIR}/.nvm ${HOME}/.nvm
+ln -s ${BASEDIR}/.zshrc ${HOME}/.zshrc
 
 # http://www.anishathalye.com/2014/08/03/managing-your-dotfiles/
