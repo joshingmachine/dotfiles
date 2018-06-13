eea149a245981209790b4be1e78255e5892e3e0a
joshingmachine <josh@joshingmachine.com>
2018-06-13 15:40:57 -0400

    Add bashrc to symlink script

--- symlink.sh
+++ symlink.sh
@@ -2,12 +2,13 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s ${BASEDIR}/.vim/ ${HOME}/.vim
+ln -s ${BASEDIR}/.bashrc ${HOME}/.bashrc
 ln -s ${BASEDIR}/.editorconfig ${HOME}/.editorconfig
 ln -s ${BASEDIR}/.gitconfig ${HOME}/.gitconfig
 ln -s ${BASEDIR}/.gitignore ${HOME}/.gitignore
 ln -s ${BASEDIR}/.nvm ${HOME}/.nvm
 ln -s ${BASEDIR}/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${BASEDIR}/.vim/ ${HOME}/.vim
 ln -s ${BASEDIR}/.zshrc ${HOME}/.zshrc
 
 # http://www.anishathalye.com/2014/08/03/managing-your-dotfiles/
