c269fd58d9b3749086ba70dcffe4c19d34b2a731
joshingmachine <josh@joshingmachine.com>
2018-07-02 20:33:13 -0400

    Working on ALE usage to lint rogue JS

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,6 +4,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s ${BASEDIR}/.bashrc ${HOME}/.bashrc
 ln -s ${BASEDIR}/.editorconfig ${HOME}/.editorconfig
+ln -s ${BASEDIR}/.eslintrc.js ${HOME}/.eslintrc.js
 ln -s ${BASEDIR}/.gitconfig ${HOME}/.gitconfig
 ln -s ${BASEDIR}/.gitignore ${HOME}/.gitignore
 ln -s ${BASEDIR}/.tmux.conf ${HOME}/.tmux.conf
