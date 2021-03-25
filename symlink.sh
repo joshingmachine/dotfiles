92df7f8e5f07ca90fdcd3d6c9f6bd15ac2b0d5fb
joshingmachine <josh@joshingmachine.com>
2021-03-25 15:09:56 -0400

    Add bash_profile to dotfiles

--- symlink.sh
+++ symlink.sh
@@ -3,6 +3,7 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ln -s "${BASEDIR}/.bashrc" "${HOME}/.bashrc"
+ln -s "${BASEDIR}/.bash_profile" "${HOME}/.bash_profile"
 ln -s "${BASEDIR}/coc-settings.json" "${HOME}/coc-settings.json"
 ln -s "${BASEDIR}/.config/" "${HOME}/.config"
 ln -s "${BASEDIR}/.editorconfig" "${HOME}/.editorconfig"
